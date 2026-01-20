3a2d0c5c5cd3e4e8dc797b904501e9e6a1e5bdf8
Meliodas-hub26 <goncalvesmanueldomingos2309@gmail.com>
2026-01-20 15:47:32 +0200

    Create Message.js

---
+++ frontend/assets/css/assets/js/Message.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const MessageSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+export default mongoose.model("Message", MessageSchema);
